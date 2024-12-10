c3c7b104d73f2f67e03b4fd5126dce27679f72f8
MrManz <vincentmanz@aol.com>
2024-12-10 18:40:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import discord
+from dotenv import load_dotenv
+
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
@@ -11,9 +14,16 @@ class MyHandler(FileSystemEventHandler):
                     pass
                 last_line = line
             print(last_line)
+            client = discord.Client()
+            load_dotenv()
+            TOKEN = os.getenv('DISCORD_TOKEN')
+            client.run(TOKEN)
+            channel = client.get_channel("1315700405430128730")
+            await channel.send(last_line)
 
 if __name__ == "__main__":
     print("Started")
+
     event_handler = MyHandler()
     observer = Observer()
     observer.schedule(event_handler, path='/factorio/logs/console.log', recursive=True)
