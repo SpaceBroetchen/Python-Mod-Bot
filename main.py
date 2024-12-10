70bd91447f52153728d0cf4eaf3897010b632f33
MrManz <vincentmanz@aol.com>
2024-12-10 17:38:56 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        print(f'File {event.src_path} has been modified')
+
+if __name__ == "__main__":
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path='/factorio/logs', recursive=True)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
