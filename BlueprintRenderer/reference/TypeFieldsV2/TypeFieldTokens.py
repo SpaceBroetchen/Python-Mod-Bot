db4255bd8ae707a2586131f3188bec77d8678c2d
SpaceBroetchen <spacebrotchen@gmail.com>
2025-02-10 20:46:04 +0100

    - some changes

--- BlueprintRenderer/reference/TypeFieldsV2/TypeFieldTokens.py
+++ BlueprintRenderer/reference/TypeFieldsV2/TypeFieldTokens.py
@@ -3,9 +3,15 @@ This is an automatic generated file for the %s type,
 the documentation can be found here https://lua-api.factorio.com/latest/types/%s.html
 """
 
-from BlueprintRenderer.reference import TypeFieldsV2
-
 '''
 
-IMPORT = "import %i\n"
+IMPORT = "import %s\n"
 
+TIDF = "\t"
+
+CLASS_NO_EXT = "class %s:\n"
+CLASS_EXT = "class %s(%s):\n"
+
+MTL_COMMENT_DELIMITER = '"""'
+
+NDS = "No description provided!"
